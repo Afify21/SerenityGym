e502e4574b494752e3e619071266c6b2d12063be
3essawyy <yousef.elessawi2004@gmail.com>
2024-12-24 00:40:40 +0200

    Final

--- SerenityGym/User/ForgotPassUser.Designer.cs
+++ SerenityGym/User/ForgotPassUser.Designer.cs
@@ -32,15 +32,14 @@
             pictureBox1 = new PictureBox();
             label1 = new Label();
             panel2 = new Panel();
-            Stafff = new Button();
+            repass = new TextBox();
+            pass = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            Userid = new TextBox();
             Userr = new Button();
             label2 = new Label();
             button1 = new Button();
-            Userid = new TextBox();
-            label3 = new Label();
-            label4 = new Label();
-            pass = new TextBox();
-            repass = new TextBox();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel2.SuspendLayout();
@@ -54,16 +53,16 @@
             panel1.Location = new Point(0, 0);
             panel1.Margin = new Padding(2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(140, 530);
+            panel1.Size = new Size(112, 424);
             panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.Sports_Dumbbell_icon;
-            pictureBox1.Location = new Point(14, 210);
+            pictureBox1.Location = new Point(11, 168);
             pictureBox1.Margin = new Padding(2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(118, 101);
+            pictureBox1.Size = new Size(94, 81);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -72,10 +71,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Century Gothic", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(185, 40);
+            label1.Location = new Point(148, 32);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(223, 39);
+            label1.Size = new Size(191, 33);
             label1.TabIndex = 1;
             label1.Text = "Serenity Gym";
             // 
@@ -87,30 +86,69 @@
             panel2.Controls.Add(label3);
             panel2.Controls.Add(Userid);
             panel2.Controls.Add(label1);
-            panel2.Controls.Add(Stafff);
             panel2.Controls.Add(Userr);
             panel2.Controls.Add(label2);
             panel2.Controls.Add(button1);
             panel2.Dock = DockStyle.Fill;
-            panel2.Location = new Point(140, 0);
+            panel2.Location = new Point(112, 0);
             panel2.Margin = new Padding(2);
             panel2.Name = "panel2";
-            panel2.Size = new Size(610, 530);
+            panel2.Size = new Size(488, 424);
             panel2.TabIndex = 1;
             // 
-            // Stafff
+            // repass
             // 
-            Stafff.BackColor = Color.FromArgb(41, 128, 185);
-            Stafff.FlatStyle = FlatStyle.Flat;
-            Stafff.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Stafff.ForeColor = Color.White;
-            Stafff.Location = new Point(341, 346);
-            Stafff.Margin = new Padding(2);
-            Stafff.Name = "Stafff";
-            Stafff.Size = new Size(148, 35);
-            Stafff.TabIndex = 8;
-            Stafff.Text = "STAFF";
-            Stafff.UseVisualStyleBackColor = false;
+            repass.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            repass.ForeColor = Color.FromArgb(41, 128, 185);
+            repass.Location = new Point(192, 190);
+            repass.Margin = new Padding(2);
+            repass.Name = "repass";
+            repass.Size = new Size(200, 28);
+            repass.TabIndex = 13;
+            // 
+            // pass
+            // 
+            pass.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            pass.ForeColor = Color.FromArgb(41, 128, 185);
+            pass.Location = new Point(192, 145);
+            pass.Margin = new Padding(2);
+            pass.Name = "pass";
+            pass.Size = new Size(200, 28);
+            pass.TabIndex = 12;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 10F);
+            label4.ForeColor = Color.FromArgb(41, 128, 185);
+            label4.Location = new Point(30, 192);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(163, 21);
+            label4.TabIndex = 11;
+            label4.Text = "Re-Enter New Pass";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 10F);
+            label3.ForeColor = Color.FromArgb(41, 128, 185);
+            label3.Location = new Point(30, 147);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(136, 21);
+            label3.TabIndex = 10;
+            label3.Text = "Enter New Pass";
+            // 
+            // Userid
+            // 
+            Userid.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Userid.ForeColor = Color.FromArgb(41, 128, 185);
+            Userid.Location = new Point(192, 94);
+            Userid.Margin = new Padding(2);
+            Userid.Name = "Userid";
+            Userid.Size = new Size(200, 28);
+            Userid.TabIndex = 9;
             // 
             // Userr
             // 
@@ -118,10 +156,10 @@
             Userr.FlatStyle = FlatStyle.Flat;
             Userr.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Userr.ForeColor = Color.White;
-            Userr.Location = new Point(37, 346);
+            Userr.Location = new Point(148, 284);
             Userr.Margin = new Padding(2);
             Userr.Name = "Userr";
-            Userr.Size = new Size(148, 35);
+            Userr.Size = new Size(118, 44);
             Userr.TabIndex = 7;
             Userr.Text = "Update";
             Userr.UseVisualStyleBackColor = false;
@@ -132,10 +170,10 @@
             label2.AutoSize = true;
             label2.Font = new Font("Century Gothic", 10F);
             label2.ForeColor = Color.FromArgb(41, 128, 185);
-            label2.Location = new Point(37, 126);
+            label2.Location = new Point(30, 101);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(70, 23);
+            label2.Size = new Size(63, 21);
             label2.TabIndex = 2;
             label2.Text = "UserID";
             // 
@@ -145,74 +183,20 @@
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Verdana", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button1.ForeColor = Color.FromArgb(41, 128, 185);
-            button1.Location = new Point(570, 0);
+            button1.Location = new Point(456, 0);
             button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(40, 40);
+            button1.Size = new Size(32, 32);
             button1.TabIndex = 0;
             button1.Text = "X";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
-            // Userid
-            // 
-            Userid.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Userid.ForeColor = Color.FromArgb(41, 128, 185);
-            Userid.Location = new Point(240, 117);
-            Userid.Margin = new Padding(2);
-            Userid.Name = "Userid";
-            Userid.Size = new Size(249, 32);
-            Userid.TabIndex = 9;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Century Gothic", 10F);
-            label3.ForeColor = Color.FromArgb(41, 128, 185);
-            label3.Location = new Point(37, 184);
-            label3.Margin = new Padding(2, 0, 2, 0);
-            label3.Name = "label3";
-            label3.Size = new Size(159, 23);
-            label3.TabIndex = 10;
-            label3.Text = "Enter New Pass";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Century Gothic", 10F);
-            label4.ForeColor = Color.FromArgb(41, 128, 185);
-            label4.Location = new Point(37, 240);
-            label4.Margin = new Padding(2, 0, 2, 0);
-            label4.Name = "label4";
-            label4.Size = new Size(191, 23);
-            label4.TabIndex = 11;
-            label4.Text = "Re-Enter New Pass";
-            // 
-            // pass
-            // 
-            pass.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            pass.ForeColor = Color.FromArgb(41, 128, 185);
-            pass.Location = new Point(240, 181);
-            pass.Margin = new Padding(2);
-            pass.Name = "pass";
-            pass.Size = new Size(249, 32);
-            pass.TabIndex = 12;
-            // 
-            // repass
-            // 
-            repass.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            repass.ForeColor = Color.FromArgb(41, 128, 185);
-            repass.Location = new Point(240, 237);
-            repass.Margin = new Padding(2);
-            repass.Name = "repass";
-            repass.Size = new Size(249, 32);
-            repass.TabIndex = 13;
-            // 
             // ForgotPassUser
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(750, 530);
+            ClientSize = new Size(600, 424);
             ControlBox = false;
             Controls.Add(panel2);
             Controls.Add(panel1);
@@ -236,7 +220,6 @@
         private Label label1;
         private Button button1;
         private Label label2;
-        private Button Stafff;
         private Button Userr;
         private TextBox Userid;
         private Label label4;
