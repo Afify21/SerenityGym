ffb7c4880b31354a84beccb0bddfdb4e31249f6b
ahmedsmoselhi <ahmedsmoselhi@gmail.com>
2024-12-23 19:47:39 +0200

    Connection String

--- SerenityGym/Receptionist.cs
+++ SerenityGym/Receptionist.cs
@@ -1,20 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using DBapplication;
+using Microsoft.VisualBasic.ApplicationServices;
 
 namespace SerenityGym
 {
     public partial class Receptionist : Form
     {
+        Controller controllerObj;
+
         public Receptionist()
         {
             InitializeComponent();
+            controllerObj = new Controller();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            int result = controllerObj.MatchingUser(Convert.ToInt16(User.Text), Pass.Text);
+            if (result == 1)
+            {
+                User user = new User(Convert.ToInt16(User.Text));
+                user.Show();
+            }
+            else
+                MessageBox.Show("Please Enter A Proper Combination");
         }
     }
 }
