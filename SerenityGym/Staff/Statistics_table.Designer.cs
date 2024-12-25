2bf8147f529d489977794d5563a6121a5527ce10
Afify21 <lolesea21@gmail.com>
2024-12-25 22:16:05 +0200

    statstable

---
+++ SerenityGym/Staff/Statistics_table.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace SerenityGym
+{
+    partial class Statistics_table
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            panel2 = new Panel();
+            Stafff = new Button();
+            Userr = new Button();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(41, 128, 185);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(78, 424);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Sports_Dumbbell_icon;
+            pictureBox1.Location = new Point(2, 177);
+            pictureBox1.Margin = new Padding(2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(73, 66);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(129, 9);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(191, 33);
+            label1.TabIndex = 1;
+            label1.Text = "Serenity Gym";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(Stafff);
+            panel2.Controls.Add(Userr);
+            panel2.Controls.Add(button1);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(78, 0);
+            panel2.Margin = new Padding(2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(522, 424);
+            panel2.TabIndex = 1;
+            // 
+            // Stafff
+            // 
+            Stafff.BackColor = Color.FromArgb(41, 128, 185);
+            Stafff.FlatStyle = FlatStyle.Flat;
+            Stafff.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Stafff.ForeColor = Color.White;
+            Stafff.Location = new Point(359, 385);
+            Stafff.Margin = new Padding(2);
+            Stafff.Name = "Stafff";
+            Stafff.Size = new Size(118, 28);
+            Stafff.TabIndex = 8;
+            Stafff.Text = "STAFF";
+            Stafff.UseVisualStyleBackColor = false;
+            // 
+            // Userr
+            // 
+            Userr.BackColor = Color.FromArgb(41, 128, 185);
+            Userr.FlatStyle = FlatStyle.Flat;
+            Userr.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Userr.ForeColor = Color.White;
+            Userr.Location = new Point(19, 385);
+            Userr.Margin = new Padding(2);
+            Userr.Name = "Userr";
+            Userr.Size = new Size(118, 28);
+            Userr.TabIndex = 7;
+            Userr.Text = "USER";
+            Userr.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Verdana", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.FromArgb(41, 128, 185);
+            button1.Location = new Point(456, 0);
+            button1.Margin = new Padding(2);
+            button1.Name = "button1";
+            button1.Size = new Size(32, 32);
+            button1.TabIndex = 0;
+            button1.Text = "X";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Statistics_table
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(600, 424);
+            ControlBox = false;
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(2);
+            Name = "Statistics_table";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Button button1;
+        private Button Stafff;
+        private Button Userr;
+    }
+}
---
+++ SerenityGym/Staff/Statistics_table.cs
@@ -0,0 +1,16 @@
+namespace SerenityGym
+{
+    public partial class Statistics_table : Form
+    {
+        public Statistics_table()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+    }
+}
