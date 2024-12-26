e707eb57244c5cda9c789844e5a9673aae92a535
Proguy2016 <yusuffahmed242@gmail.com>
2024-12-26 17:52:39 +0200

    suiiii

--- SerenityGym/Staff/Receptionist.Designer.cs
+++ SerenityGym/Staff/Receptionist.Designer.cs
@@ -29,27 +29,21 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            button3 = new Button();
             upd = new ComboBox();
             button2 = new Button();
             Userr = new Button();
             Stafff = new Button();
             pictureBox1 = new PictureBox();
             panel2 = new Panel();
+            button7 = new Button();
             pictureBox4 = new PictureBox();
-            Stafff = new Button();
-            Userr = new Button();
-          /*  pictureBox3 = new PictureBox();
-            pictureBox2 = new PictureBox();*/
             label2 = new Label();
             button1 = new Button();
-            button7 = new Button();
-            button3 = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-/*            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();*/
             SuspendLayout();
             // 
             // panel1
@@ -57,32 +51,39 @@
             panel1.BackColor = Color.FromArgb(41, 128, 185);
             panel1.Controls.Add(button3);
             panel1.Controls.Add(upd);
+            panel1.Controls.Add(Stafff);
             panel1.Controls.Add(button2);
             panel1.Controls.Add(Userr);
-            panel1.Controls.Add(Stafff);
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
             panel1.Margin = new Padding(2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(300, 530);
-            panel1.Size = new Size(209, 424);
+            panel1.Size = new Size(167, 424);
             panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(41, 128, 185);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(11, 263);
+            button3.Margin = new Padding(2);
+            button3.Name = "button3";
+            button3.Size = new Size(138, 67);
+            button3.TabIndex = 11;
+            button3.Text = "CLOCK OUT";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
             // 
             // upd
             // 
-         /*   label1.AutoSize = true;
-            label1.Font = new Font("Century Gothic", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(36, 235);
-            label1.Margin = new Padding(2, 0, 2, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(223, 39);
-            label1.TabIndex = 1;
-            label1.Text = "Serenity Gym";*/
             upd.FormattingEnabled = true;
             upd.Items.AddRange(new object[] { "User", "Self" });
-            upd.Location = new Point(31, 109);
+            upd.Location = new Point(21, 87);
+            upd.Margin = new Padding(2, 2, 2, 2);
             upd.Name = "upd";
-            upd.Size = new Size(151, 28);
+            upd.Size = new Size(122, 28);
             upd.TabIndex = 10;
             // 
             // button2
@@ -91,10 +92,10 @@
             button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button2.ForeColor = Color.White;
-            button2.Location = new Point(31, 231);
+            button2.Location = new Point(11, 194);
             button2.Margin = new Padding(2);
             button2.Name = "button2";
-            button2.Size = new Size(147, 61);
+            button2.Size = new Size(138, 65);
             button2.TabIndex = 9;
             button2.Text = "SEND NOTIFICATION";
             button2.UseVisualStyleBackColor = false;
@@ -106,10 +107,10 @@
             Userr.FlatStyle = FlatStyle.Flat;
             Userr.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Userr.ForeColor = Color.White;
-            Userr.Location = new Point(31, 37);
+            Userr.Location = new Point(11, 27);
             Userr.Margin = new Padding(2);
             Userr.Name = "Userr";
-            Userr.Size = new Size(151, 57);
+            Userr.Size = new Size(138, 56);
             Userr.TabIndex = 7;
             Userr.Text = "UPDATE";
             Userr.UseVisualStyleBackColor = false;
@@ -121,10 +122,10 @@
             Stafff.FlatStyle = FlatStyle.Flat;
             Stafff.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Stafff.ForeColor = Color.White;
-            Stafff.Location = new Point(31, 154);
+            Stafff.Location = new Point(11, 129);
             Stafff.Margin = new Padding(2);
             Stafff.Name = "Stafff";
-            Stafff.Size = new Size(147, 60);
+            Stafff.Size = new Size(138, 61);
             Stafff.TabIndex = 8;
             Stafff.Text = "SIGN UP USER";
             Stafff.UseVisualStyleBackColor = false;
@@ -133,13 +134,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.Sports_Dumbbell_icon;
-            pictureBox1.Location = new Point(95, 56);
-            pictureBox1.Margin = new Padding(2);
-            pictureBox1.Location = new Point(63, 133);
+            pictureBox1.Location = new Point(50, 106);
             pictureBox1.Margin = new Padding(2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(118, 101);
-            pictureBox1.Size = new Size(246, 159);
+            pictureBox1.Size = new Size(197, 127);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -148,94 +146,50 @@
             // 
             panel2.Controls.Add(button7);
             panel2.Controls.Add(pictureBox4);
-            panel2.Controls.Add(Stafff);
-            panel2.Controls.Add(Userr);
-       /*     panel2.Controls.Add(pictureBox3);
-            panel2.Controls.Add(pictureBox2);*/
             panel2.Controls.Add(pictureBox1);
             panel2.Controls.Add(label2);
             panel2.Controls.Add(button1);
             panel2.Dock = DockStyle.Fill;
-            panel2.Location = new Point(209, 0);
-            panel2.Margin = new Padding(2);
-            panel2.Location = new Point(300, 0);
+            panel2.Location = new Point(167, 0);
             panel2.Margin = new Padding(2);
             panel2.Name = "panel2";
-            panel2.Size = new Size(450, 530);
-            panel2.Size = new Size(391, 424);
+            panel2.Size = new Size(433, 424);
             panel2.TabIndex = 1;
             panel2.Paint += panel2_Paint;
             // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(41, 128, 185);
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.ForeColor = Color.White;
+            button7.Location = new Point(0, 389);
+            button7.Margin = new Padding(2);
+            button7.Name = "button7";
+            button7.Size = new Size(101, 33);
+            button7.TabIndex = 19;
+            button7.Text = "Log Out";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
             // pictureBox4
             // 
-/*            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");*/
-            pictureBox4.Location = new Point(3, 3);
+            pictureBox4.Location = new Point(2, 2);
+            pictureBox4.Margin = new Padding(2, 2, 2, 2);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(28, 37);
+            pictureBox4.Size = new Size(22, 30);
             pictureBox4.TabIndex = 8;
             pictureBox4.TabStop = false;
             pictureBox4.Click += pictureBox4_Click;
-            // 
-            // Stafff
-            // 
-            Stafff.BackColor = Color.FromArgb(41, 128, 185);
-            Stafff.FlatStyle = FlatStyle.Flat;
-            Stafff.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Stafff.ForeColor = Color.White;
-            Stafff.Location = new Point(240, 318);
-            Stafff.Margin = new Padding(2);
-            Stafff.Name = "Stafff";
-            Stafff.Size = new Size(148, 35);
-            Stafff.TabIndex = 8;
-            Stafff.Text = "STAFF";
-            Stafff.UseVisualStyleBackColor = false;
-            // 
-            // Userr
-            // 
-            Userr.BackColor = Color.FromArgb(41, 128, 185);
-            Userr.FlatStyle = FlatStyle.Flat;
-            Userr.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Userr.ForeColor = Color.White;
-            Userr.Location = new Point(62, 318);
-            Userr.Margin = new Padding(2);
-            Userr.Name = "Userr";
-            Userr.Size = new Size(148, 35);
-            Userr.TabIndex = 7;
-            Userr.Text = "USER";
-            Userr.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox3
-            // 
-         /*   pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(265, 212);
-            pictureBox3.Margin = new Padding(2);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(86, 99);
-            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox3.TabIndex = 5;
-            pictureBox3.TabStop = false;*/
-            // 
-            // pictureBox2
-            // 
-        /*    pictureBox2.Image = Properties.Resources.User;
-            pictureBox2.Location = new Point(109, 235);
-            pictureBox2.Margin = new Padding(2);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(62, 52);
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox2.TabIndex = 3;
-            pictureBox2.TabStop = false;*/
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label2.ForeColor = Color.FromArgb(41, 128, 185);
-            label2.Location = new Point(24, 118);
-            label2.Location = new Point(99, 22);
+            label2.Location = new Point(79, 18);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(179, 39);
             label2.Size = new Size(165, 31);
             label2.TabIndex = 2;
             label2.Text = "Receptionist";
@@ -246,52 +200,20 @@
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Verdana", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button1.ForeColor = Color.FromArgb(41, 128, 185);
-            button1.Location = new Point(410, 0);
-            button1.Margin = new Padding(2);
-            button1.Location = new Point(349, 0);
+            button1.Location = new Point(279, 0);
             button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(40, 40);
+            button1.Size = new Size(32, 32);
             button1.TabIndex = 0;
             button1.Text = "X";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
-            // button7
-            // 
-            button7.BackColor = Color.FromArgb(41, 128, 185);
-            button7.FlatStyle = FlatStyle.Flat;
-            button7.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button7.ForeColor = Color.White;
-            button7.Location = new Point(0, 489);
-            button7.Margin = new Padding(2);
-            button7.Name = "button7";
-            button7.Size = new Size(126, 41);
-            button7.TabIndex = 19;
-            button7.Text = "Log Out";
-            button7.UseVisualStyleBackColor = false;
-            button7.Click += button7_Click;
-            // 
-            // button3
-            // 
-            button3.BackColor = Color.FromArgb(41, 128, 185);
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button3.ForeColor = Color.White;
-            button3.Location = new Point(31, 317);
-            button3.Margin = new Padding(2);
-            button3.Name = "button3";
-            button3.Size = new Size(147, 61);
-            button3.TabIndex = 11;
-            button3.Text = "CLOCK OUT";
-            button3.UseVisualStyleBackColor = false;
-            button3.Click += button3_Click;
-            // 
             // Receptionist
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(750, 530);
+            ClientSize = new Size(600, 424);
             ControlBox = false;
             Controls.Add(panel2);
             Controls.Add(panel1);
@@ -305,8 +227,6 @@
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-      /*      ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();*/
             ResumeLayout(false);
         }
 
